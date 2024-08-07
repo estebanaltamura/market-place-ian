27b0b4dbcce0f06c6a12410b5291b9f9b9aea3b2
Andres <esteban.altamura@gmail.com>
2024-08-07 18:44:00 +0200

    update

--- src/api/scrape.js
+++ src/api/scrape.js
@@ -1,26 +1,17 @@
-import express from 'express';
 import puppeteer from 'puppeteer';
-import cors from 'cors';
 
-const app = express();
-app.use(cors()); // Configurar CORS para permitir todas las solicitudes
-
-app.get('/scrape', async (req, res) => {
+export default async function handler(req, res) {
   try {
-    const browser = await puppeteer.launch();
-
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     await page.goto('https://www.khanacademy.org/profile/idev0x00', { waitUntil: 'networkidle0' });
     const content = await page.content();
-    console.log(content);
     await browser.close();
-    res.send(content);
+    res.status(200).send(content);
   } catch (error) {
     res.status(500).send('Error fetching the page');
   }
-});
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+}
